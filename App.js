1785d95a74bcbdaf6ceeb5e8cc7e493c5c0c928b
addison527 <148710689+addison527@users.noreply.github.com>
2024-08-15 15:15:47 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -8,8 +8,10 @@ import About from './components/pages/About';
 import ClassSchedule from './components/pages/ClassSchedule';
 import Contact from './components/pages/Contact';
 import Footer from './components/Footer';
-import Demonstration from './components/pages/Demos';
+import Demos from './components/pages/Demos';
 import FollowAlong from './components/pages/FollowAlong';
+import Blogs from './components/pages/Blogs';
+import Merch from './components/pages/Merch';
 
 function App() {
   return (
@@ -20,9 +22,11 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/class-schedule" element={<ClassSchedule />} />
-            <Route path="/demonstrations" element={<Demonstration />} />
+            <Route path="/demonstrations" element={<Demos />} />
             <Route path="/follow-along" element={<FollowAlong />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/merch" element={<Merch />} />
+            <Route path="/blogs" element={<Blogs/>} />
           </Routes>
         <Footer/>
       </Router>
