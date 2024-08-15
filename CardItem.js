588a3aa8561b9b633d93e49949538561e490fe75
Vicky Villeta <viv226@lehigh.edu>
2024-08-15 15:32:00 -0400

    Update CardItem.js

--- CardItem.js
+++ CardItem.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function CardItem(props) {
+
+  const scrollToTop = () => { // Fixes the weird thing where the page loads in the middle
+    window.scrollTo({
+      top: 0,
+      behavior: 'instant'
+    });
+  };
+
   return (
     <>
       <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
+        <Link className='cards__item__link' to={props.path} onClick={scrollToTop}>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
-           <img
+            <img
               className='cards__item__img'
               alt='Taichi Image'
               src={props.src}
-            /> 
+            />
           </figure>
           <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.text}</h5>
@@ -21,4 +29,5 @@ function CardItem(props) {
     </>
   );
 }
- export default CardItem;
+
+export default CardItem;
